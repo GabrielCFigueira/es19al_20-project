9da0e8642fdedf3e32788d0e97ae8efbc243cb83
JoaoPmargaco <joao.margaco@tecnico.ulisboa.pt>
2019-03-07 15:01:57 +0000

    Migrate SellerToPaySpockTest closes #33

---
+++ tax/src/test/groovy/pt/ulisboa/tecnico/softeng/tax/domain/SellerToPaySpockTest.groovy
@@ -0,0 +1,90 @@
+package pt.ulisboa.tecnico.softeng.tax.domain 
+
+import org.joda.time.LocalDate 
+
+import spock.lang.Shared
+
+import pt.ulisboa.tecnico.softeng.tax.domain.Buyer 
+import pt.ulisboa.tecnico.softeng.tax.domain.IRS 
+import pt.ulisboa.tecnico.softeng.tax.domain.Invoice 
+import pt.ulisboa.tecnico.softeng.tax.domain.ItemType 
+import pt.ulisboa.tecnico.softeng.tax.domain.Seller 
+import pt.ulisboa.tecnico.softeng.tax.exception.TaxException 
+
+public class SellerToPaySpockTest extends SpockRollbackTestAbstractClass {
+	@Shared def SELLER_NIF = "123456789" 
+	@Shared def BUYER_NIF = "987654321" 
+	@Shared def FOOD = "FOOD" 
+	@Shared def TAX = 10 
+	def date = LocalDate.parse("2018-02-13") 
+
+	def seller 
+	def buyer 
+	def itemType 
+
+	@Override
+	def populate4Test() {
+		def irs = IRS.getIRSInstance() 
+		seller = new Seller(irs, SELLER_NIF, "José Vendido", "Somewhere") 
+		buyer = new Buyer(irs, BUYER_NIF, "Manuel Comprado", "Anywhere") 
+		itemType = new ItemType(irs, FOOD, TAX) 
+	}
+
+	def 'success'() {
+        when: 'creating multiple invoices'
+            new Invoice(100, date, itemType, seller, buyer) 
+            new Invoice(100, date, itemType, seller, buyer) 
+            new Invoice(50, date, itemType, seller, buyer) 
+		    def value = seller.toPay(2018) 
+        then: 'checking value toPay in the known year'
+		    25.0 == value 
+	}
+
+	def 'yearWithoutInvoices'() {
+        when: 'creating multiple invoices'
+            new Invoice(100,  date,  itemType,  seller,  buyer) 
+            new Invoice(100,  date,  itemType,  seller,  buyer) 
+            new Invoice(50,  date,  itemType,  seller,  buyer) 
+        then: 'checking value toPay in a past year'
+		    0.0 == seller.toPay(2015) 
+	}
+
+	def 'noInvoices'() {
+        when: 'using default value toPay'
+		    def value =  seller.toPay(2018) 
+        then: 'checking value toPay'
+		    0.0 == value 
+	}
+
+	def 'before1970'() {
+        when: 'creating invoices before 1970'
+            new Invoice(100, new LocalDate(1969, 02, 13),  itemType,  seller,  buyer) 
+            new Invoice(50, new LocalDate(1969, 02, 13),  itemType,  seller,  buyer) 
+            def value =  seller.toPay(1969) 
+            0.0 == value
+        then: 'throwing exception'
+            thrown(TaxException)
+		   
+	}
+
+	def 'equal1970'() {
+        when: 'creating invoices in 1970'
+            new Invoice(100, new LocalDate(1970, 02, 13),  itemType,  seller,  buyer) 
+            new Invoice(50, new LocalDate(1970, 02, 13),  itemType,  seller,  buyer) 
+            def value =  seller.toPay(1970) 
+        then: "checking value toPay"
+		    15.0 == value
+	}
+
+	def 'ignoreCancelled'() {
+        when: 'creating multiple invoices and cancelling one'
+		    new Invoice(100,  date,  itemType,  seller,  buyer) 
+            def invoice = new Invoice(100,  date,  itemType,  seller,  buyer) 
+            new Invoice(50,  date,  itemType,  seller,  buyer) 
+            invoice.cancel() 
+		    def value =  seller.toPay(2018) 
+        then: 'checking value toPay'
+		    15.0 == value
+	}
+
+}
