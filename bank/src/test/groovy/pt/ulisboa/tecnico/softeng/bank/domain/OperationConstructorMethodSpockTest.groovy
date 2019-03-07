ae9b2aa8c1a8ce6b9b5faa338778ff4ca590ad67
Pedro Matias <pedro.matias.carvalho@tecnico.ulisboa.pt>
2019-03-07 15:36:42 +0000

    Migrate OperationConstructorMethodSpockTest closes #48

---
+++ bank/src/test/groovy/pt/ulisboa/tecnico/softeng/bank/domain/OperationConstructorMethodSpockTest.groovy
@@ -0,0 +1,53 @@
+package pt.ulisboa.tecnico.softeng.bank.domain
+
+import spock.lang.Shared
+import spock.lang.Unroll
+import pt.ulisboa.tecnico.softeng.bank.domain.Operation.Type
+import pt.ulisboa.tecnico.softeng.bank.exception.BankException
+
+class OperationConstructorMethodSpockTest extends SpockRollbackTestAbstractClass {
+	private Bank bank
+	@Shared private Account account
+
+	def populate4Test() {
+		bank = new Bank("Money", "BK01")
+		Client client = new Client(bank, "António")
+		account = new Account(bank, client)
+	}
+
+	def 'success'() {
+        when:	'creating a new operation'
+		    Operation operation = new Operation(Type.DEPOSIT, this.account, 1000);
+
+        then:	'should succeed'
+		    operation.getReference().startsWith(this.bank.getCode()) == true
+		    operation.getReference().length() > Bank.CODE_SIZE
+		    Type.DEPOSIT == operation.getType()
+		    account == operation.getAccount()
+		    1000 == operation.getValue()
+		    operation.getTime() != null
+		    operation == bank.getOperation(operation.getReference())
+	}
+
+	@Unroll('Operation: #type, #account, #money')
+	def 'exceptions'() {
+		when: 'creating a new operation with invalid arguments'
+		    new Operation(type, _account, money)
+		then:	'throws an exception'
+			thrown(BankException)
+		where:
+			type 		  | _account | money
+			null          |  account | 1000
+			Type.WITHDRAW | null     | 1000
+			Type.DEPOSIT  | account  | 0
+			Type.WITHDRAW | account  | -1000
+	}
+
+	def 'oneAmount'() {
+        when:	'creating a new operation'
+		    Operation operation = new Operation(Type.DEPOSIT, account, 1)
+        then:	'should succeed'
+		    operation == bank.getOperation(operation.getReference())
+	}
+
+}
