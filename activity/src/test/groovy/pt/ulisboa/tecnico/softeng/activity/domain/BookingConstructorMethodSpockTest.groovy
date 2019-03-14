6fa1d702722df8960f0d03af8651188067f479ae
DiogoFariaFernandes <diogo7ff@gmail.com>
2019-03-14 11:42:16 +0000

    Fix: Added @Unroll, data tables and comments in BookingConstructorMethodSpockTest closes #13

--- activity/src/test/groovy/pt/ulisboa/tecnico/softeng/activity/domain/BookingConstructorMethodSpockTest.groovy
+++ activity/src/test/groovy/pt/ulisboa/tecnico/softeng/activity/domain/BookingConstructorMethodSpockTest.groovy
@@ -1,124 +1,98 @@
-package pt.ulisboa.tecnico.softeng.activity.domain;
+package pt.ulisboa.tecnico.softeng.activity.domain
 
-import org.joda.time.LocalDate;
+import org.joda.time.LocalDate
 
-import spock.lang.Shared;
+import spock.lang.Shared
+import spock.lang.Unroll
 
-import pt.ulisboa.tecnico.softeng.activity.exception.ActivityException;
+import pt.ulisboa.tecnico.softeng.activity.exception.ActivityException
 
-import mockit.FullVerifications;
+import mockit.FullVerifications
 
 
 class BookingContructorMethodSpockTest extends SpockRollbackTestAbstractClass {
-	def private provider;
-	def private offer;
-	@Shared def private final AMOUNT = 30;
-	@Shared def private final IBAN = "IBAN";
-	@Shared def private final NIF = "123456789";
+	@Shared def provider
+	@Shared def offer
+	def AMOUNT = 30
+	@Shared def IBAN = "IBAN"
+	@Shared def NIF = "123456789"
 
 	@Override
 	def populate4Test() {
-		provider = new ActivityProvider("XtremX", "ExtremeAdventure", "NIF", IBAN);
-		def activity = new Activity(provider, "Bush Walking", 18, 80, 3);
-
-		def begin = new LocalDate(2016, 12, 19);
-		def end = new LocalDate(2016, 12, 21);
-		offer = new ActivityOffer(activity, begin, end, AMOUNT);
+		provider = new ActivityProvider("XtremX", "ExtremeAdventure", "NIF", IBAN)
+		def activity = new Activity(provider, "Bush Walking", 18, 80, 3)
+		def begin = new LocalDate(2016, 12, 19)
+		def end = new LocalDate(2016, 12, 21)
+		offer = new ActivityOffer(activity, begin, end, AMOUNT)
 	}
 
 
 	def 'success'() {
-		when:
-		def booking = new Booking(this.provider, this.offer, NIF, IBAN);
-		then:
-		true == booking.getReference().startsWith(provider.getCode());
-		true == booking.getReference().length() > ActivityProvider.CODE_SIZE;
-		1 == offer.getNumberActiveOfBookings();
-		
-		NIF == booking.getBuyerNif();
-		IBAN == booking.getIban();
-		AMOUNT == booking.getAmount();
+		when:'when creating a new Booking'
+			def booking = new Booking(provider, offer, NIF, IBAN)
+		then: 'should succeed'
+			true == booking.getReference().startsWith(provider.getCode())
+			true == booking.getReference().length() > ActivityProvider.CODE_SIZE
+			1 == offer.getNumberActiveOfBookings()
+			
+			NIF == booking.getBuyerNif()
+			IBAN == booking.getIban()
+			AMOUNT == booking.getAmount()
+	}
+	@Unroll('BookingConstructorMethod:#provider_tab, #offer_tab, #NIF_tab, #IBAN_tab')
+	def 'nullFullVerifications'(){
+		when:'when creating Booking'
+			new Booking(provider_tab, offer_tab, NIF_tab, IBAN_tab)
+			new FullVerifications() {}
+		then:'throws ActivityException'
+			thrown(ActivityException)
+		where:
+			provider_tab	| offer_tab 	| NIF_tab	| IBAN_tab
+			null			| offer			| NIF		| IBAN
+			provider		| null 			| NIF 		| IBAN
+			null			| offer 		| null 		| IBAN
+			null			| offer 		| NIF		| null
+				
+	}
+	
+	@Unroll('BookingConstructorMethod:#provider_tab, #offer_tab, #NIF_tab, #IBAN_tab')
+	def 'empty'(){
+		when:'when creating Booking'
+			new Booking(provider_tab, offer_tab, NIF_tab, IBAN_tab)
+		then:'throws ActivityException'
+			thrown(ActivityException)
+		where:
+			provider_tab	| offer_tab 	| NIF_tab	| IBAN_tab
+			provider		| null 			| NIF 		| "     "
+			provider		| null 			| "     " 	| IBAN
 	}
 
-	def 'nullProvider'() {
-		when:
-		new Booking(null, offer, NIF, IBAN);
-
-		new FullVerifications() {
-		};
-		then:
-		thrown(ActivityException);
-	}
-
-	def 'nullOffer'() {
-		when:
-		new Booking(provider, null, NIF, IBAN);
-
-		new FullVerifications() {
-		};
-		then:
-		thrown(ActivityException);
-	}
-
-	def 'nullNIF'() {
-		when:
-		new Booking(null, offer, null, IBAN);
-
-		new FullVerifications() {
-		};
-		then:
-		thrown(ActivityException);
-	}
-
-	def 'emptyIBAN'() {
-		when:
-		new Booking(provider, null, NIF, "     ");
-		then:
-		thrown(ActivityException);
-	}
-
-	def 'nullIBAN'() {
-		when:
-		new Booking(null, offer, NIF, null);
-
-		new FullVerifications() {
-		};
-		then:
-		thrown(ActivityException);
-	}
-
-	def 'emptyNIF'() {
-		when:
-		new Booking(provider, null, "     ", IBAN);
-		then:
-		thrown(ActivityException);
-	}
 
 	def 'bookingEqualCapacity'() {
-		given:
-		new Booking(provider, offer, NIF, IBAN);
-		new Booking(provider, offer, NIF, IBAN);
-		new Booking(provider, offer, NIF, IBAN);
-		when:
-		new Booking(provider, offer, NIF, IBAN);
-		then:
-		thrown(ActivityException); 
-		and:
-		3 == offer.getNumberActiveOfBookings();
+		given:'given three bookings'
+			new Booking(provider, offer, NIF, IBAN)
+			new Booking(provider, offer, NIF, IBAN)
+			new Booking(provider, offer, NIF, IBAN)
+		when:'when creating a forth'
+			new Booking(provider, offer, NIF, IBAN)
+		then:'throw ActivtyException'
+			thrown(ActivityException)
+		and:'assert the expected number(3)'
+			3 == offer.getNumberActiveOfBookings()
 		
 	}
 
 
 	def 'bookingEqualCapacityButHasCancelled'() {
-		given:
-		new Booking(provider, offer, NIF, IBAN);
-		new Booking(provider, offer, NIF, IBAN);
-		def booking = new Booking(provider, offer, NIF, IBAN);
-		when:
-		booking.cancel();		
-		new Booking(provider, offer, NIF, IBAN);
-		then:
-		3 == offer.getNumberActiveOfBookings();
+		given:'given three bookings'
+			new Booking(provider, offer, NIF, IBAN)
+			new Booking(provider, offer, NIF, IBAN)
+			def booking = new Booking(provider, offer, NIF, IBAN)
+		when:'when cancelling'
+			booking.cancel()
+			new Booking(provider, offer, NIF, IBAN)
+		then:'assert the expected number(3)'
+			3 == offer.getNumberActiveOfBookings()
 	}
 
 }
