83a992d00515ced816f666f854b3efaffe68ca5c
DiogoEusebio <deusebio98@outlook.pt>
2019-03-14 17:16:49 +0000

    correct sintax problems

--- tax/src/test/groovy/pt/ulisboa/tecnico/softeng/tax/domain/ItemTypeConstructorSpockTest.groovy
+++ tax/src/test/groovy/pt/ulisboa/tecnico/softeng/tax/domain/ItemTypeConstructorSpockTest.groovy
@@ -2,63 +2,62 @@ package pt.ulisboa.tecnico.softeng.tax.domain
 
 import pt.ulisboa.tecnico.softeng.tax.exception.TaxException
 import spock.lang.Shared
-import spock.lang.Specification
 import spock.lang.Unroll
 
 import static org.junit.Assert.assertNotNull;
 
 class ItemTypeConstructorSpockTest extends SpockRollbackTestAbstractClass {
-    @Shared String CAR = "CAR";
-	@Shared int TAX = 23;
+    @Shared def CAR = "CAR"
+	@Shared def TAX = 23
 
-	@Shared def IRS irs;
+	@Shared def irs
 
 	@Override
 	def populate4Test() {
-		this.irs = IRS.getIRSInstance();
+		irs = IRS.getIRSInstance()
 	}
 
     def 'success'() {
         when: 'creating a new ItemType'
-        def IRS irs = IRS.getIRSInstance();
-        def itemType = new ItemType(irs, CAR, TAX);
+        def IRS irs = IRS.getIRSInstance()
+        def itemType = new ItemType(irs, CAR, TAX)
 
         then: 'should succeed'
         itemType.getName() == CAR
         itemType.getTax() == TAX
-        assertNotNull IRS.getIRSInstance().getItemTypeByName(CAR)
+        null != IRS.getIRSInstance().getItemTypeByName(CAR)
         irs.getItemTypeByName(CAR) == itemType
     }
 
     def 'uniqueName'() {
         given:
-        def itemType = new ItemType(this.irs, CAR, TAX)
+        def itemType = new ItemType(irs, CAR, TAX)
         
         when:
-        new ItemType(this.irs, CAR, TAX)
+        new ItemType(irs, CAR, TAX)
         
         then:
         thrown(TaxException)
         itemType == IRS.getIRSInstance().getItemTypeByName(CAR)
     }
 
-    def 'zeroTax'() {
-        new ItemType(this.irs, CAR, 0)
+    def zeroTax() {
+        new ItemType(irs, CAR, 0)
     }
 
     @Unroll('ItemType: #irs, #itemType, #tax')
     def 'exceptions'() {
         when:'creating an ItemType with invalid arguments'
-        new ItemType(this.irs, itemType, tax)
+        new ItemType(irs, itemType, tax)
 
         then:
         thrown(TaxException)
 
         where: 'cases where some arguments are null or invalid'
-        irs      | itemType | tax
-        this.irs | null     | TAX
-        this.irs | ""       | TAX
-        this.irs | CAR      | -34
+        irs | itemType | tax
+        irs | null     | TAX
+        irs | ""       | TAX
+        irs | CAR      | -34
 
 
     }
